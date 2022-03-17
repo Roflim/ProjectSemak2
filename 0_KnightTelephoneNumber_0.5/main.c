23c578cf621d67bb892cc3cfc17df96ff83074ff
Roflim <diman74150@gmail.com>
2022-03-17 12:32:56 +0500

    First commit

--- 0_KnightTelephoneNumber_0.5/main.c
+++ 0_KnightTelephoneNumber_0.5/main.c
@@ -7,70 +7,50 @@ int TelefonHorses(int Num, long long unsigned int count, int Numbers, int A) {
 	}
 	if (A <= Num) {
 		if (Numbers == 1) {
-			count = TelefonHorses(Num, count + 1, 6, ++A);
-			--A;
-			count = TelefonHorses(Num, count + 1, 8, ++A);
-			--A;
+			count = TelefonHorses(Num, count + 1, 6, A + 1);
+			count = TelefonHorses(Num, count + 1, 8, A + 1);
 		}
 		if (Numbers == 2) {
-			count = TelefonHorses(Num, count + 1, 9, ++A);
-			--A;
-			count = TelefonHorses(Num, count + 1, 7, ++A);
-			--A;
+			count = TelefonHorses(Num, count + 1, 9, A + 1);
+			count = TelefonHorses(Num, count + 1, 7, A + 1);
 		}
 		if (Numbers == 3) {
-			count = TelefonHorses(Num, count + 1, 4, ++A);
-			--A;
-			count = TelefonHorses(Num, count + 1, 8, ++A);
-			--A;
+			count = TelefonHorses(Num, count + 1, 4, A + 1);
+			count = TelefonHorses(Num, count + 1, 8, A + 1);
 		}
 		if (Numbers == 4) {
-			count = TelefonHorses(Num, count + 1, 3, ++A);
-			--A;
-			count = TelefonHorses(Num, count + 1, 9, ++A);
-			--A;
-			count = TelefonHorses(Num, count + 1, 0, ++A);
-			--A;
+			count = TelefonHorses(Num, count + 1, 3, A + 1);
+			count = TelefonHorses(Num, count + 1, 9, A + 1);
+			count = TelefonHorses(Num, count + 1, 0, A + 1);
 		}
 		if (Numbers == 5) {
 			return count;
 		}
 		if (Numbers == 6) {
-			count = TelefonHorses(Num, count + 1, 1, ++A);
-			--A;
-			count = TelefonHorses(Num, count + 1, 7, ++A);
-			--A;
-			count = TelefonHorses(Num, count + 1, 0, ++A);
-			--A;
+			count = TelefonHorses(Num, count + 1, 1, A + 1);
+			count = TelefonHorses(Num, count + 1, 7, A + 1);
+			count = TelefonHorses(Num, count + 1, 0, A + 1);
 		}
 		if (Numbers == 7) {
-			count = TelefonHorses(Num, count + 1, 2, ++A);
-			--A;
-			count = TelefonHorses(Num, count + 1, 6, ++A);
-			--A;
+			count = TelefonHorses(Num, count + 1, 2, A + 1);
+			count = TelefonHorses(Num, count + 1, 6, A + 1);
 		}
 		if (Numbers == 9) {
-			count = TelefonHorses(Num, count + 1, 2, ++A);
-			--A;
-			count = TelefonHorses(Num, count + 1, 4, ++A);
-			--A;
+			count = TelefonHorses(Num, count + 1, 2, A + 1);
+			count = TelefonHorses(Num, count + 1, 4, A + 1);
 		}
 		if (Numbers == 0) {
-			if(count == 0){ return 0; }
+			if (count == 0) { return 0; }
 			else {
-				count = TelefonHorses(Num, count + 1, 4, ++A);
-				--A;
-				count = TelefonHorses(Num, count + 1, 6, ++A);
-				--A;
+				count = TelefonHorses(Num, count + 1, 4, A + 1);
+				count = TelefonHorses(Num, count + 1, 6, A + 1);
 			}
 		}
 		if (Numbers == 8) {
 			if (count == 0) { return 0; }
 			else {
-				count = TelefonHorses(Num, count + 1, 1, ++A);
-				--A;
-				count = TelefonHorses(Num, count + 1, 3, ++A);
-				--A;
+				count = TelefonHorses(Num, count + 1, 1, A + 1);
+				count = TelefonHorses(Num, count + 1, 3, A + 1);
 			}
 		}
 	}
