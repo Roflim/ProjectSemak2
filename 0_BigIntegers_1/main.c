9b6a982d36d574d20b1724a1886cc3a18eb01f32
Roflim <diman74150@gmail.com>
2022-03-17 11:06:10 +0500

    First commit

--- 0_BigIntegers_1/main.c
+++ 0_BigIntegers_1/main.c
@@ -5,49 +5,59 @@ struct BigInt { int* Mass; int CountMass; };
 
 int SumInt(int* FirstMass, int* SeconMass, int FirstCountMass, int SeconCountMass) {
 	int tmp = 0, tmpSecon = 0;
+	int* SummaMass = (int*)malloc((FirstCountMass + SeconCountMass) * sizeof(int));
+	for (int i = 0; i < (FirstCountMass + SeconCountMass); ++i) {
+		SummaMass[i] = '\0';
+	}
 	for (int i = (FirstCountMass - 1), j = (SeconCountMass - 1); i >= 0; --i) {
 		if (j >= 0) { tmpSecon = SeconMass[j]; --j; }
 		else tmpSecon = 0;
 		tmp = FirstMass[i] + tmpSecon + tmp;
 		if (tmp >= 10) {
-			FirstMass[i] = tmp % 10;
+			SummaMass[i + SeconCountMass] = tmp % 10;
 			tmp = tmp / 10;
 		}
 	}
+	return SummaMass;
 }
 
 
 int main() {
-	struct BigInt First, Secon;
+	struct BigInt First, Secon, Summa;
 	First.CountMass = 15; Secon.CountMass = 1;
-	int* FirstMass = (int*)malloc(First.CountMass * sizeof(int));
+	First.Mass = (int*)malloc(First.CountMass * sizeof(int));
 	for (int i = 0; i < First.CountMass; ++i) {
-		FirstMass[i] = '\0';
+		First.Mass[i] = '\0';
 	}
-	int* SeconMass = (int*)malloc(First.CountMass * sizeof(int));
+	Secon.Mass = (int*)malloc(First.CountMass * sizeof(int));
 	for (int i = 0; i < Secon.CountMass; ++i) {
-		SeconMass[i] = '\0';
+		Secon.Mass[i] = '\0';
 	}
 
 	for (int i = 0; i < First.CountMass; ++i) {
-		FirstMass[i] = 9;
+		First.Mass[i] = 9;
 	}
-	SeconMass[0] = 1;
+	Secon.Mass[0] = 1;
 	for (int i = 0; i < First.CountMass; ++i) {
-		printf("%d", FirstMass[i]);
+		printf("%d", First.Mass[i]);
 	}
 	printf("\n");
 	for (int i = 0; i < Secon.CountMass; ++i) {
-		printf("%d", SeconMass[i]);
+		printf("%d", Secon.Mass[i]);
 	}
 	printf("\n");
-	SumInt(FirstMass, SeconMass, First.CountMass, Secon.CountMass);
+
+	Summa.Mass = SumInt(First.Mass, Secon.Mass, First.CountMass, Secon.CountMass);
+
 	for (int i = 0; i < First.CountMass; ++i) {
-		printf("%d", FirstMass[i]);
+		printf("%d", First.Mass[i]);
 	}
 	printf("\n");
 	for (int i = 0; i < Secon.CountMass; ++i) {
-		printf("%d", SeconMass[i]);
+		printf("%d", Secon.Mass[i]);
+	}
+	for (int i = 0; i < First.CountMass + Secon.CountMass; ++i) {
+		printf("%d", Summa.Mass[i]);
 	}
 	printf("\n");
 }
